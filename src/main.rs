e9bb504cefa7577a09ce857b59e34437e99cd5f9
yanghan <h_go@qq.com>
2025-12-02 11:07:19 +0800

    feat(cli): Implement core file reading and pattern matching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+use clap::Parser;
+use std::{fs, path::PathBuf};
+
+#[derive(Parser)]
+#[command(version,about,long_about = None)]
+struct Grep {
+    pattern: String,
+
+    path: PathBuf,
+}
+fn main() -> Result<()> {
+    let grep = Grep::parse();
+
+    let contents = fs::read_to_string(&grep.path)
+        .with_context(|| format!("could not read file `{}`", grep.path.display()))?;
+    // println!("read fille:{:?}", contents);
+
+    for line in contents.lines() {
+        if line.contains(&grep.pattern) {
+            println!("{}", line);
+        }
+    }
+    Ok(())
+    // println!("Searching for: {}", grep.pattern);
+    // println!("In file: {}", grep.path.display());
 }
